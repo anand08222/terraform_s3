04fbc67b98fb23b00f3f695f63e6f27e0932c44b
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-06-22 13:27:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,15 @@ terraform {
  }
 }
 
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 /* resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
