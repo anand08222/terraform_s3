3036b6cd69907f199ee971146024dfa11682282f
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-06-22 11:51:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,22 +7,6 @@ terraform {
  }
 }
 
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::terraform-bucket-anand2"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject"],
-      "Resource": "arn:aws:s3:::terraform-bucket-anand2/myapp/dev/terraform.tfstate"
-    }
-  ]
-}
-
 
 /* resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
