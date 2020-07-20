438538bfc056d826fa243afa8e294e6a9db851f7
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-07-20 12:33:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,27 +22,3 @@ provider "aws" {
   region = "us-east-1"
   } 
 
-#NEW CODE
- 
- resource "aws_s3_bucket" "bucket08222_anand32" {
-  bucket = "user-bucket-anand018222"
-  acl    = "private"
-
-  tags = {
-    Name = "My bucket"
-  }
-}
-locals {
-  s3_origin_id = "myS3Origin"
-}
-
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = "${aws_s3_bucket.bucket08222_anand32.bucket_regional_domain_name}"
-    origin_id   = "${local.s3_origin_id}"
-
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-    }
-  }
-}
