fb501a7b568d20576c8a0deb7d454aab140dd555
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-07-23 16:57:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,15 @@ resource "aws_s3_bucket" "bucket08222_anand2" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket" "bucket08222_anand2" {
+  bucket = "user-bucket-anand01082gfjhf22"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
 # Use AWS Terraform provider
 provider "aws" {
